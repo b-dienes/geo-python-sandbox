629affb400492ddde6aeb800fb2b64ee0ab96018
b-dienes <244570204+b-dienes@users.noreply.github.com>
2026-02-02 11:46:45 +0100

    Orchestrate requests_demo in main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import logging
 from dataclasses import dataclass
 import geopandas as gpd
-import geopandas_demo
 from pyproj import CRS
+import numpy as np
+
+import geopandas_demo
+import requests_demo
 
 logging.basicConfig(
     level=logging.INFO,
@@ -42,24 +45,29 @@ def run_vector_pipeline(UserInput: UserInput) -> gpd.GeoDataFrame:
 
 
 def prepare_raster_bounding_boxes(parks_clipped: gpd.GeoDataFrame) -> dict:
-    logger.info("Entering Mercator reprojection")
+    logger.info("Entered bbox reprojection to Mercator")
     target_crs = CRS.from_user_input("EPSG:3857")
 
     if parks_clipped.crs != target_crs:
         parks_clipped = parks_clipped.to_crs(target_crs)
 
-    parks_clipped['BBOX_MINX'] = parks_clipped.bounds['minx']
-    parks_clipped['BBOX_MINY'] = parks_clipped.bounds['miny']
-    parks_clipped['BBOX_MAXX'] = parks_clipped.bounds['maxx']
-    parks_clipped['BBOX_MAXY'] = parks_clipped.bounds['maxy']
-    parks_reduced = parks_clipped[['fid', 'PARKNAME', 'BBOX_MINX', 'BBOX_MINY', 'BBOX_MAXX', 'BBOX_MAXY']]
+    bboxes = parks_clipped.geometry.bounds.to_numpy().tolist()
 
-    parks_dict = parks_reduced.to_dict('list')
-
-    logger.info("Parks dict: %s", parks_dict)
+    parks_dict = [
+        {"fid": i, "parkname": n, "bbox": b}
+        for i, n, b in zip(parks_clipped["fid"], parks_clipped["PARKNAME"], bboxes)
+    ]
 
     return parks_dict
+
+def run_raster_pipeline(parks_dict):
+    logger.info("Entering requests demo")
+
+    current_park = parks_dict[0]
+    naip_response = requests_demo.download_naip(current_park)
+    requests_demo.save_naip_response(current_park, naip_response)
 
 filled_user_input = user_input()
 parks_clipped = run_vector_pipeline(filled_user_input)
 parks_dict = prepare_raster_bounding_boxes(parks_clipped)
+run_raster_pipeline(parks_dict)
